96f7c0800d53226e5f75476ee00a947379aeea4a
Thomas Järvstrand <tjarvstrand@gmail.com>
2022-01-03 12:32:54 +0100

    Ukulele layouts and macos symlinks for gnu tools

--- .bashrc.d/00-osx.bash
+++ .bashrc.d/00-osx.bash
@@ -1,16 +1,21 @@
 
 osx () {
 
-    alias config='/usr/bin/git --git-dir=$HOME/.config/cfg.mac/ --work-tree=$HOME'
+    if which asdf > /dev/null
+    then
+        export ASDF_DATA_DIR="${HOME}/.cache/asdf"
+        source "/usr/local/lib/asdf.sh"
+        export PATH="$ASDF_DATA_DIR/shims:$PATH"
+    fi
 
-if which asdf > /dev/null
-then
-    export ASDF_DATA_DIR="${HOME}/.cache/asdf"
-    source "/usr/local/lib/asdf.sh"
-    export PATH="$ASDF_DATA_DIR/shims:$PATH"
-fi
+    source '/Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash'
 
-source '/Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash'
+    mkdir -p $HOME/bin
+    for c in {grep,readlink,sed}; do
+        if [ ! -f $HOME/bin/$c ]; then
+            ln -s /usr/local/bin/g$c $HOME/bin/$c
+        fi
+    done
 
 }
 
