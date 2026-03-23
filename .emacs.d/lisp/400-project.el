ebc8f587f4d20cbad76d87403591bc1a18a064d9
Thomas Järvstrand <tjarvstrand@gmail.com>
2026-03-23 13:57:28 +0100

    Emacs: Add .jj to vc project root markers

--- .emacs.d/lisp/400-project.el
+++ .emacs.d/lisp/400-project.el
@@ -1,1 +1,2 @@
+(add-to-list 'project-vc-extra-root-markers ".jj")
 (add-to-list 'project-vc-extra-root-markers ".project-root")
--- .emacs.d/lisp/500-dart.el
+++ .emacs.d/lisp/500-dart.el
@@ -61,130 +61,9 @@
                :program "lib/main.dart"
                ))
 
-(defconst my-dart--test-call-kinds '("group" "test" "testWidgets"))
-
 (defun my-dart--ensure-dart-parser ()
   (unless (treesit-parser-list nil 'dart)
     (treesit-parser-create 'dart)))
-
-(defun my-dart--dart-string-literal-content (node)
-  "Return NODE string literal content without delimiters, or nil."
-  (when (and node (string= (treesit-node-type node) "string_literal"))
-    (let ((raw (treesit-node-text node t)))
-      ;; fixme, this is stupid
-      (cond
-       ((string-prefix-p "r\"\"\"" raw)
-        (substring raw 4 -3))
-       ((string-prefix-p "R\"\"\"" raw)
-        (substring raw 4 -3))
-       ((string-prefix-p "r'''" raw)
-        (substring raw 4 -3))
-       ((string-prefix-p "R'''" raw)
-        (substring raw 4 -3))
-       ((string-prefix-p "\"\"\"" raw)
-        (substring raw 3 -3))
-       ((string-prefix-p "'''" raw)
-        (substring raw 3 -3))
-       ((string-prefix-p "r\"" raw)
-        (substring raw 2 -1))
-       ((string-prefix-p "R\"" raw)
-        (substring raw 2 -1))
-       ((string-prefix-p "r'" raw)
-        (substring raw 2 -1))
-       ((string-prefix-p "R'" raw)
-        (substring raw 2 -1))
-       ((string-prefix-p "\"" raw)
-        (substring raw 1 -1))
-       ((string-prefix-p "'" raw)
-        (substring raw 1 -1))
-       (t raw)))))
-
-(defun my-dart--test-call-info-from-node (node)
-  "Return plist for NODE if it's a supported Dart test call, else nil."
-  (when (string= (treesit-node-type node) "expression_statement")
-    (let* ((callee (treesit-node-child node 0))
-           (selector (treesit-node-child node 1))
-           (kind (and callee (string= (treesit-node-type callee) "identifier")
-                      (treesit-node-text callee t))))
-      (when (and kind (member kind my-dart--test-call-kinds)
-                 selector (string= (treesit-node-type selector) "selector"))
-        (let* ((argument-part (treesit-node-child selector 0))
-               (arguments (and argument-part
-                               (string= (treesit-node-type argument-part) "argument_part")
-                               (treesit-node-child argument-part 0)))
-               (first-argument (and arguments
-                                    (string= (treesit-node-type arguments) "arguments")
-                                    (treesit-node-child arguments 1)))
-               (string-node (and first-argument
-                                 (string= (treesit-node-type first-argument) "argument")
-                                 (treesit-node-child first-argument 0)))
-               (name (my-dart--dart-string-literal-content string-node)))
-          (when name
-            (list :kind kind
-                  :name name
-                  :start (treesit-node-start node))))))))
-
-(defun my-dart--enclosing-calls-at-point ()
-  "Return enclosing group/test/testWidgets calls from outer to inner."
-  (my-dart--ensure-dart-parser)
-  (let ((node (treesit-node-at (point) 'dart))
-        calls)
-    (while node
-      (when-let ((call (my-dart--test-call-info-from-node node)))
-        (push call calls))
-      (setq node (treesit-node-parent node)))
-    calls))
-
-(defun my-dart-group-at-point ()
-  "Return the full enclosing group path at point, or nil."
-  (let (groups)
-    (dolist (call (my-dart--enclosing-calls-at-point))
-      (when (string= (plist-get call :kind) "group")
-        (push (plist-get call :name) groups)))
-    (setq groups (nreverse groups))
-    (when groups
-      (mapconcat #'identity groups " "))))
-
-(defun my-dart-test-name-at-point ()
-  "Return the enclosing test name at point, or nil."
-  (let ((nearest (car (last (my-dart--enclosing-calls-at-point)))))
-    (when nearest
-      (let ((kind (plist-get nearest :kind)))
-        (when (or (string= kind "test") (string= kind "testWidgets"))
-          (plist-get nearest :name))))))
-
-(defun my-dart-test-at-point ()
-  "Return full test path at point, or full group path if not in a test."
-  (let ((group-name (my-dart-group-at-point))
-        (test-name (my-dart-test-name-at-point)))
-    (cond
-     ((and group-name test-name) (format "%s %s" group-name test-name))
-     (test-name test-name)
-     (group-name group-name))))
-
-(add-to-list 'dape-configs
-             `(dart-test
-               modes (dart-ts-mode)
-               ensure dape-ensure-command
-               command "dart"
-               command-args ("debug_adapter" "--test")
-               command-cwd dape-command-cwd
-               :type "dart"
-               :request "launch"
-               :name "Dart Test"
-               :cwd ,(lambda () (f-dirname (or (my-dart--nearest-file "pubspec.yaml") (buffer-file-name))))
-               :program ,(lambda () (buffer-file-name))
-               :args ["--concurrency=1"]
-               :console "terminal"
-               ))
-
-(defun my-dart-debug-test-at-point ()
-  "Debug the Dart test at point, or all tests in file if no test found."
-  (interactive)
-  (my-dape 'dart-test
-           (when-let ((test-name (my-dart-test-at-point)))
-             (list :args (vector "--concurrency=1" "--plain-name" test-name)))))
-
 
 (defun my-dart-organize-imports ()
   (save-restriction
@@ -212,36 +91,6 @@
       (signal (car last-no-action-error) (cdr last-no-action-error)))
     (eglot-format-buffer)))
 
-(defun my-dart-test-program ()
-  (or (executable-find "flutter") (executable-find "dart")))
-
-(defvar my-dart-last-test nil)
-
-(defun my-dart-run-test-at-point (&optional prefix)
-  (interactive "P")
-  (let* ((test (or (my-dart-test-at-point) (error "No test at point!"))))
-    (setq my-dart-last-test (cons buffer-file-name test))
-    (my-dart--run-tests prefix buffer-file-name test)))
-
-(defun my-dart-run-last-test (&optional prefix)
-  (interactive "P")
-  (unless my-dart-last-test
-    (error "No last test to run"))
-  (my-dart--run-tests prefix (car my-dart-last-test) (cdr my-dart-last-test)))
-
-
-(defun my-dart-run-tests-in-file (&optional prefix)
-  (interactive "P")
-  (setq my-dart-last-test (list buffer-file-name))
-  (my-dart--run-tests prefix buffer-file-name nil))
-
-(defun my-dart--run-tests (prefix test-file test-name)
-  (interactive "P")
-  (let ((program (my-dart-test-program))
-        (reporter (if prefix "expanded" "compact"))
-        (name-arg (if test-name (format " --name \"%s\"" test-name) "")))
-  (compile (format "%s test%s -r %s %s" program name-arg reporter test-file))))
-
 (defun my-dart--terminate-statement ()
   (interactive)
   (save-excursion
@@ -251,15 +100,6 @@
       (delete-char -1))
     (let ((last-command-event ?\;))
       (call-interactively #'self-insert-command))))
-
-(define-key dart-ts-mode-map (kbd "C-c t t") #'my-dart-run-test-at-point)
-(define-key dart-ts-mode-map (kbd "C-c t T") #'my-dart-run-tests-in-file)
-(define-key dart-ts-mode-map (kbd "C-c t d") #'my-dart-debug-test-at-point)
-;;(define-key dart-ts-mode-map (kbd "C-c t D") #'my-dart-debug-tests-in-file) fixme
-(define-key dart-ts-mode-map (kbd "C-c t r") #'my-dart-run-last-test)
-
-(define-key dart-ts-mode-map (kbd "C-x ;") #'my-dart--terminate-statement)
-
 
 (defun my-dart-mode-hook ()
   (company-mode)
