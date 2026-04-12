636214dfb692e7e52d3875fa35bbe639df1c5ec2
Thomas Järvstrand <tjarvstrand@gmail.com>
2026-04-12 09:35:56 +0200

    Emacs: yaml-mode and markdown-mode fixes

--- .emacs.d/lisp/500-markdown.el
+++ .emacs.d/lisp/500-markdown.el
@@ -1,1 +1,14 @@
 (use-package markdown-mode)
+
+
+(with-eval-after-load 'yaml-mode
+  (setq markdown-command "pandoc")
+  (setq markdown-split-window-direction 'right)
+  (keymap-unset markdown-mode-map "M-p" t)
+  (keymap-unset markdown-mode-map "M-n" t))
+
+(defun my-markdown-mode-hook ()
+  (display-line-numbers-mode 1))
+
+(add-hook 'markdown-mode-hook 'my-markdown-mode-hook)
+
--- .emacs.d/lisp/500-yaml.el
+++ .emacs.d/lisp/500-yaml.el
@@ -1,6 +1,10 @@
 (use-package yaml-mode)
 
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-(add-hook 'yaml-mode-hook
-          (lambda ()
-            (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+(defun my-yaml-mode-hook ()
+  (display-line-numbers-mode 1)
+  (define-key yaml-mode-map "\C-m" 'newline-and-indent)
+)
+
+(add-hook 'yaml-mode-hook 'my-yaml-mode-hook)
