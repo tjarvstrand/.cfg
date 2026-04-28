2d919c3fa0f0a7aeb77d1febe36d08bc1dc9bb9d
Thomas Järvstrand <tjarvstrand@gmail.com>
2026-04-28 08:03:00 +0200

    Emacs: Switch to project's tab when switching to new buffer

--- .emacs.d/lisp/033-tabs.el
+++ .emacs.d/lisp/033-tabs.el
@@ -2,33 +2,21 @@
 (setq tab-bar-select-tab-modifiers '(super))
 (tab-bar-mode 1)
 
-;; (use-package tabspaces
-;;   :hook (after-init . tabspaces-mode) ;; use this only if you want the minor-mode loaded at startup.
-;;   :commands (tabspaces-switch-or-create-workspace
-;;              tabspaces-open-or-create-project-and-workspace)
-;;   :custom
-;;   (tabspaces-use-filtered-buffers-as-default t)
-;;   (tabspaces-default-tab "Default")
-;;   ; (tabspaces-remove-to-default t)
-;;   ;; (tabspaces-include-buffers '("*scratch*"))
-;;   ;; (tabspaces-exclude-buffers '("*Messages*" "*Compile-Log*"))  ; Additional buffers to exclude
-;;   ;; sessions
-;;   ;; (tabspaces-session t)
-;;   ;(tabspaces-session-auto-restore t)
-;;   ;; additional options
-;;   (tabspaces-fully-resolve-paths t)  ; Resolve relative project paths to absolute
-;;   (tpab-bar-new-tab-choice "*scratch*"))
-
 (use-package otpp
   :after project
   :init
   (setq otpp-find-file-integration t) ; route find-file to project tab
   (otpp-mode 1))
 
+(defvar my--otpp-detaching nil)
+
 (defun my/otpp-detach-current-buffer-to-project-tab (&rest _)
-  (when-let ((buf (current-buffer)))
-    (when (ignore-errors (project-current nil (buffer-local-value 'default-directory buf)))
-      (ignore-errors (otpp-detach-buffer-to-tab buf)))))
+  "When opening a new buffer make sure to switch to that buffer's project's tab"
+  (unless my--otpp-detaching
+    (let ((my--otpp-detaching t))
+      (when-let ((buf (current-buffer)))
+        (when (ignore-errors (project-current nil (buffer-local-value 'default-directory buf)))
+          (ignore-errors (otpp-detach-buffer-to-tab buf)))))))
 
 (advice-add 'switch-to-buffer :after #'my/otpp-detach-current-buffer-to-project-tab)
 
